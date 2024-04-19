ebeea290c54a273a917bd502eed55775a86eafd6
Piyush Chhawachharia <pichhawachharia891@gmail.com>
2024-04-19 17:06:40 +0530

    Modified Error handling File

---
+++ src/main/java/ErrorHandling.java
@@ -0,0 +1,64 @@
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.EmptyStackException;
+import java.util.Optional;
+
+class ErrorHandling {
+
+    void handleErrorByThrowingIllegalArgumentException() {
+        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    }
+
+    void handleErrorByThrowingIllegalArgumentExceptionWithDetailMessage(String message) {
+        throw new IllegalArgumentException(message);
+    }
+
+    void handleErrorByThrowingAnyCheckedException() throws IOException {
+        throw new IOException();
+    }
+
+    void handleErrorByThrowingAnyCheckedExceptionWithDetailMessage(String message) throws IOException {
+        throw new IOException(message);
+    }
+
+    void handleErrorByThrowingAnyUncheckedException() {
+        // Delete this statement and write your own implementation.
+        throw new RuntimeException();
+    }
+
+    void handleErrorByThrowingAnyUncheckedExceptionWithDetailMessage(String message) {
+        // Delete this statement and write your own implementation.
+        throw new RuntimeException(message);
+    }
+
+    void handleErrorByThrowingCustomCheckedException() throws CustomCheckedException {
+        throw new CustomCheckedException();
+
+    }
+
+    void handleErrorByThrowingCustomCheckedExceptionWithDetailMessage(String message) throws CustomCheckedException {
+        throw new CustomCheckedException(message);
+
+    }
+
+    void handleErrorByThrowingCustomUncheckedException() {
+        throw new CustomUncheckedException();
+    }
+
+    void handleErrorByThrowingCustomUncheckedExceptionWithDetailMessage(String message) {
+        throw new CustomUncheckedException(message);
+    }
+
+    Optional<Integer> handleErrorByReturningOptionalInstance(String integer) {
+
+
+        try{
+            int num=Integer.parseInt(integer);
+            return Optional.of(num);
+        }catch(Exception e){
+            return Optional.empty();
+        }
+
+    }
+
+}
